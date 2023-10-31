984ac306299fa8b0202c7f7b5c68b67b088515b4
bora997 <148833877+bora997@users.noreply.github.com>
2023-10-31 13:24:41 +0700

    Student

---
+++ grails-app/controllers/schoolsystem/StudentController.groovy
@@ -0,0 +1,21 @@
+package schoolsystem
+
+import grails.converters.JSON
+
+class StudentController {
+
+    createStudent(){
+        Student createStu = new Student()
+        def requestJSON = request.JSON
+        createStu.firstName = requestJSON.firstName
+        createStu.lastName = requestJSON.lastName
+        createStu.age = requestJSON.age
+        createStu.gender = requestJSON.gender
+        createStu.email = requestJSON.email
+        createStu.dateOfBirth = requestJSON.dateOfBirth
+        createStu.save(flush:true)
+        render createStu as JSON
+
+
+    }
+}

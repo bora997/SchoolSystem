d8df0e3e527134ec3aac20f8e7051359bada0721
bora997 <148833877+bora997@users.noreply.github.com>
2023-10-31 13:18:47 +0700

    Student

--- grails-app/domain/schoolsystem/Student.groovy
+++ grails-app/domain/schoolsystem/Student.groovy
@@ -1,8 +1,13 @@
 package schoolsystem
 
 class Student {
-    String name
+    String firstName
+    String lastName
     String age
+    String gender
+    String email
+    String contactNumber
+    Date dateOfBirth
 
     static constraints = {
     }
